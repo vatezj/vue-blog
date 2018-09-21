b4234b1909dac19bf9d3dc98af9c8868211062ab
vate <2324757255@qq.com>
2018-09-21 11:14:00 +0800

    'fix'

--- vswoft/config/beans/base.php
+++ vswoft/config/beans/base.php
@@ -26,6 +26,17 @@ return [
 
         ],
     ],
+    'sessionManager' => [
+        'class' => \Swoft\Session\SessionManager::class,
+        'config' => [
+            'driver' => 'redis',
+            'name' => 'SWOFT_SESSION_ID',
+            'lifetime' => 1800,
+            'expire_on_close' => false,
+            'encrypt' => false,
+            'storage' => '@runtime/sessions',
+        ],
+    ],
     'view' => [
         'viewsPath' => '@resources/views/',
     ],
