d29e082269f96839fd679202473d5e7b6dd4f8bb
vate <2324757255@qq.com>
2018-09-26 17:39:20 +0800

    'fix'

--- vswoft/app/Middlewares/CorsMiddleware.php
+++ vswoft/app/Middlewares/CorsMiddleware.php
@@ -36,7 +36,9 @@ class CorsMiddleware implements MiddlewareInterface
     private function configResponse(ResponseInterface $response)
     {
         return $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080,https://www.vate.ren,https://.vate.ren')
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+            ->withHeader('Access-Control-Allow-Origin', 'https://.vate.ren')
+            ->withHeader('Access-Control-Allow-Origin', 'https://www.vate.ren')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     }
