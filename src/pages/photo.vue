d264ffad67df12730ba06e18539c7a9f0160e6aa
vate <2324757255@qq.com>
2018-10-08 17:31:52 +0800

    'f'

--- src/pages/photo.vue
+++ src/pages/photo.vue
@@ -118,17 +118,7 @@ html {
   width: 100%;
 }
 h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
   color: #42b983;
 }
